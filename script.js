c49a2fb72caa4acfbe7395409a689b48c0f9d3ef
mahipanc <mahipancholik@gmail.com>
2026-01-21 21:57:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Fade-in scroll animation
 document.addEventListener('DOMContentLoaded', () => {
-  const faders = document.querySelectorAll('.fade-in-section');
+    const faders = document.querySelectorAll('.fade-in');
 
-  const appearOptions = { threshold: 0.1 };
-  const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) return;
-      entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
-    });
-  }, appearOptions);
+    const appearOptions = { threshold: 0.1 };
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        });
+    }, appearOptions);
 
-  faders.forEach(fader => appearOnScroll.observe(fader));
+    faders.forEach(fader => appearOnScroll.observe(fader));
 });
